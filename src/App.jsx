c2551e81941755066a31c70d3db2c9c9b51161a1
SahilMalgundkar24 <sahilmalgundkar321@gmail.com>
2024-06-02 23:04:02 +0530

    Added the login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import "./index.css"
 import UserSec from './Components/UserSec'
 import MessageSec from './Components/MessageSec'
+import LoginSign from './Components/LoginSign'
 
 const App = () => {
   return (
     <>
-      <div className='h-screen w-screen flex'>
+      {/* <div className='h-screen w-screen flex'>
         <UserSec/>
         <MessageSec/>
+      </div> */}
+      <div>
+        <LoginSign/>
       </div>
         
     </>
