d31f06efa06499c928995aa403d01cddbe4e0103
SahilMalgundkar24 <sahilmalgundkar321@gmail.com>
2024-06-03 12:02:09 +0530

    Ui changed and added the notification section done with the frontend

--- src/Components/UserSec.jsx
+++ src/Components/UserSec.jsx
@@ -4,8 +4,14 @@ import { IoIosNotifications } from "react-icons/io";
 const UserSec = () => {
   const signout = () => {
     auth.signOut();
+    
   };
   const [adduser, setadduser] = useState(false);
+  const [showmodal,setshowmodal]=useState(false);
+
+  const modalclicked = ()=>{
+    setshowmodal(!showmodal)
+  }
 
   const buttonclicked = () => {
     setadduser(!adduser);
@@ -17,7 +23,7 @@ const UserSec = () => {
           <div>
             Chatting App
           </div>
-          <div>
+          <div onClick={modalclicked}>
             <IoIosNotifications />
           </div>
         </div>
@@ -28,8 +34,30 @@ const UserSec = () => {
           <div className="w-full h-12 hover:bg-black text-white poppins flex items-center text-xl px-5 ">
             Sahil Malgundkar
           </div>
+          
+          
         </div>
-        <div className="w-full height10 flex items-center justify-center p-4">
+
+        <div className="w-full height10 flex flex-col items-center justify-center px-3 py-2">
+          <div className="w-full h-full">
+            {adduser && (
+              <div className="w-full h-full flex justify-between">
+                <input
+                  type="text"
+                  placeholder="Enter username to add a friend"
+                  className=" h-3/4 w-3/4 p-2 border rounded-lg"
+                />
+                <button className=" w-1/5 h-3/4 color3 rounded-lg text-white poppins">
+                  Add
+                </button>
+              </div>
+            )}
+          </div>
+          
+        </div>
+
+
+        <div className="w-full height10 flex items-center justify-center px-4 py-2">
           <button
             className="w-full h-full color3 rounded-lg text-white poppins"
             onClick={signout}
@@ -37,28 +65,31 @@ const UserSec = () => {
             signout
           </button>
         </div>
-        <div className="w-full height20 flex flex-col items-center justify-center p-4">
-          <div className="w-full h-1/2">
-            {adduser && (
-              <div className="w-full h-full">
-                <input
-                  type="text"
-                  placeholder="Enter username"
-                  className="w-3/4 p-2 border rounded-lg"
-                />
-                <button className="w-1/4 h-full color3 rounded-lg text-white poppins">
-                  Add
-                </button>
+        
+
+        {showmodal && (
+          <div className="absolute inset-0 h-70 mt-16 w-1/4 color1 flex flex-col">
+            <div className="w-full h-12 hover:bg-black flex justify-between items-center px-5 ">
+              <div className="text-white poppins text-xl">
+                Anushka
               </div>
-            )}
+              <button className=" color6 p-1 w-20 rounded-lg">
+                Accept
+              </button>
+            </div>
+            
+
+            <div className="w-full h-12 hover:bg-black flex justify-between items-center px-5 ">
+              <div className="text-white poppins text-xl">
+                Rupali(Lmao)
+              </div>
+              <button className=" color6 p-1 w-20 rounded-lg">
+                Accept
+              </button>
+            </div>
+
           </div>
-          <button
-            className="w-full h-1/2 color3 rounded-lg text-white poppins"
-            onClick={buttonclicked}
-          >
-            Add a User
-          </button>
-        </div>
+        )}
       </div>
     </>
   );
