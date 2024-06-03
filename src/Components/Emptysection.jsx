78652a441d8fe16d85571f288e9944d448a189bc
SahilMalgundkar24 <sahilmalgundkar321@gmail.com>
2024-06-03 21:36:10 +0530

    Edited empty little

--- src/Components/Emptysection.jsx
+++ src/Components/Emptysection.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Emptysection = (closeempty) => {
+const Emptysection = () => {
   return (
     <>
-        <div className='h-full w-3/4 color2'>
-
+        <div className='h-full w-3/4 color2 text-white flex justify-center text-xl items-center poppins'>
+            Send and recieve messages
         </div>
     </>
   )
