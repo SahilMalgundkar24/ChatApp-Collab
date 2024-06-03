e256fc07024ecfb1baafc1580f2dc8fd7b21aa1c
Tejas <tejasg263@gmail.com>
2024-06-03 18:22:21 +0530

    FriendList partial done

--- src/Components/UserSec.jsx
+++ src/Components/UserSec.jsx
@@ -5,6 +5,8 @@ import {
   query,
   where,
   getDocs,
+  updateDoc,
+  arrayUnion,
 } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -14,6 +16,7 @@ import { IoIosNotifications } from "react-icons/io";
 const UserSec = () => {
   const [user] = useAuthState(auth);
   const [chatList, setchatList] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const unsubscribe = onSnapshot(doc(db, "friendList", user.uid), (doc) => {
@@ -28,17 +31,6 @@ const UserSec = () => {
     auth.signOut();
   };
 
-  const [adduser, setadduser] = useState(false);
-  const [search, setSearch] = useState("");
-  const [showmodal, setshowmodal] = useState(false);
-
-  const modalclicked = () => {
-    setshowmodal(!showmodal);
-  };
-
-  const buttonclicked = () => {
-    setadduser(!adduser);
-  };
   const handleSearch = async (e) => {
     e.preventDefault();
     try {
@@ -48,7 +40,16 @@ const UserSec = () => {
 
       if (!querySnapshot.empty) {
         alert("user found");
-        console.log(querySnapshot.docs[0].data());
+        const userData = querySnapshot.docs[0].data();
+        try {
+          const docRef = doc(db, "friendList", user.uid);
+          await updateDoc(docRef, {
+            list: arrayUnion(userData), // Add the new item using arrayUnion
+          });
+          console.log("Item added successfully!");
+        } catch (error) {
+          console.error("Error adding item:", error);
+        }
       } else {
         alert("user not found");
       }
@@ -63,13 +64,16 @@ const UserSec = () => {
         <div className="w-full height10 flex mt-2 px-5 poppins-bold text-2xl text-white">
           Chatting App
         </div>
+
         <div className="w-full scrollabe height70 overflow-y-scroll ">
-          <div className="w-full h-12 hover:bg-black text-white poppins flex items-center text-xl px-5 ">
-            Tejas Gawde
-          </div>
-          <div className="w-full h-12 hover:bg-black text-white poppins flex items-center text-xl px-5 ">
-            Sahil Malgundkar
-          </div>
+          {chatList?.list?.map((chat) => (
+            <div
+              key={chat.uid}
+              className="w-full h-12 hover:bg-black text-white poppins flex items-center text-xl px-5 "
+            >
+              {chat.username}
+            </div>
+          ))}
         </div>
 
         <div className="w-full height10 flex flex-col items-center justify-center px-3 py-2">
@@ -99,20 +103,6 @@ const UserSec = () => {
             signout
           </button>
         </div>
-
-        {showmodal && (
-          <div className="absolute inset-0 h-70 mt-16 w-1/4 color1 flex flex-col">
-            <div className="w-full h-12 hover:bg-black flex justify-between items-center px-5 ">
-              <div className="text-white poppins text-xl">Anushka</div>
-              <button className=" color6 p-1 w-20 rounded-lg">Accept</button>
-            </div>
-
-            <div className="w-full h-12 hover:bg-black flex justify-between items-center px-5 ">
-              <div className="text-white poppins text-xl">Rupali</div>
-              <button className=" color6 p-1 w-20 rounded-lg">Accept</button>
-            </div>
-          </div>
-        )}
       </div>
     </>
   );
